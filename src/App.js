b1e1264eda29f09b62ba97aa941e1bdbe9ad819f
Ben Hernandez <benjammin234@gmail.com>
2020-07-23 15:15:49 -0700

    Choices working

--- src/App.js
+++ src/App.js
@@ -3,22 +3,41 @@ import { Route, Switch } from "react-router-dom";
 import Rules from "./Rules";
 import Choices from "./Choices";
 import Battlefield from "./Battlefield";
+import Context from "./Context";
 
-function App() {
-  return (
-    <div className="App">
-      <main>
-        <header>
-          <h1>Rock Paper Scissors</h1>
-        </header>
-        <Switch>
-          <Route exact path="/" component={Rules} />
-          <Route path="/choices" component={Choices} />
-          <Route path="/battlefield" component={Battlefield} />
-        </Switch>
-      </main>
-    </div>
-  );
+export default class App extends React.Component {
+  static contextType = Context;
+
+  state = {
+    userChoice: "",
+  };
+
+  makeChoice = (choice) => {
+    this.setState({
+      userChoice: choice,
+    });
+  };
+
+  render() {
+    const value = {
+      makeChoice: this.makeChoice,
+    };
+
+    return (
+      <Context.Provider value={value}>
+        <div className="App">
+          <main>
+            <header>
+              <h1>Rock Paper Scissors</h1>
+            </header>
+            <Switch>
+              <Route exact path="/" component={Rules} />
+              <Route path="/choices" component={Choices} />
+              <Route path="/battlefield" component={Battlefield} />
+            </Switch>
+          </main>
+        </div>
+      </Context.Provider>
+    );
+  }
 }
-
-export default App;
