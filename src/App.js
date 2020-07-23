9173d3a9a36d0635ee4fb5c319ae1901705a5ddc
Ben Hernandez <benjammin234@gmail.com>
2020-07-23 14:36:30 -0700

    Putting together some basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Rules from "./Rules";
+import Choices from "./Choices";
+import Battlefield from "./Battlefield";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main>
+        <header>
+          <h1>Rock Paper Scissors</h1>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Rules} />
+          <Route path="/choices" component={Choices} />
+          <Route path="/battlefield" component={Battlefield} />
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Battlefield.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Battlefield extends React.Component {
+  render() {
+    return <div className="Battlefield"></div>;
+  }
+}
